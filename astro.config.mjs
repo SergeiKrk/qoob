b85ae216867dd367710ad350e973306d949d7636
skryukov <krjukov_s@mail.ru>
2023-08-17 18:00:34 +0400

    установил @astrojs/prefetch ---

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import image from "@astrojs/image";
-import prefetch from '@astrojs/prefetch';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
@@ -10,31 +9,16 @@ import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
 import purgecss from 'astro-purgecss';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    purgecss({
-      fontFace: true,
-      keyframes: true,
-      safelist: ['random', 'yep', 'button', /^nav-/],
-      blocklist: ['usedClass', /^nav-/],
-      content: [
-        process.cwd() + '/src/**/*.{astro,vue}', // Watching astro and vue sources (for SSR, read the note below)
-      ],
-    }),
-  prefetch({
-    // Only prefetch links with an href that begins with `/products` or `/coupons`
-    intentSelector: ["a[href^='/about']", "a[href^='/blog']", "a[href^='/blog.*']"],
-
-    // Use a string to prefetch a single selector
-    // intentSelector: "a[href^='/products']"
-  }),
-],
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   integrations: [
+    prefetch(),
+    purgecss(),
     react(),
     sitemap(),
     tailwind({
